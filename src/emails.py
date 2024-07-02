368189a3eca869bc95c2951eaafe42dc4ce9fb80
Victory <victorykasende@gmail.com>
2024-07-02 08:45:04 +0100

    initial commit

---
+++ src/emails.py
@@ -0,0 +1,37 @@
+import smtplib
+from src import configurations, message_email_html
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+title = "STATISTIQUE DOMOTIQUE MAMAN SERVICE"
+destination = "victoirekasende133@gmail.com"
+provenance = "MAMAN SERVICE"
+
+
+def envoyer_email(email_destinataire, sujet, message, provenance):
+    multipart_message = MIMEMultipart()
+    multipart_message["Subject"] = sujet
+    multipart_message["From"] = provenance
+    multipart_message["To"] = email_destinataire
+
+    html_part = MIMEText(message, "html")
+    multipart_message.attach(html_part)
+    
+    serveur_mail = smtplib.SMTP(configurations.config_server, configurations.config_server_port)
+    serveur_mail.starttls()
+    serveur_mail.login(configurations.config_email, configurations.config_password)
+    serveur_mail.sendmail(configurations.config_email, email_destinataire, multipart_message.as_string())
+    serveur_mail.quit()
+    
+
+def main():
+    envoyer_email(destination, title, 
+        message_email_html.message_email, provenance=provenance)
+
+
+if __name__ == "__main__":
+    main()
+    print("Reussi !")
+
+     
+
